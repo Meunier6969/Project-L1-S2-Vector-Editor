45896e1115f5f6b848b3e09a5d04530a5ef648f0
Meunier6969 <antoine.meunier@efrei.net>
2023-04-18 17:40:15 +0200

    continue the menu

--- main.c
+++ main.c
@@ -20,15 +20,17 @@ int main(int argc, char const *argv[])
     initialize_buffer(buffer, WIDTH, HEIGHT);
     /*************************************/
 
-    int choice = -1;
 
     Shape* listShapes[100];
+    int nodeList[6] = {2,3,5,7,11,13};
     int currentID = 0;
 
+    int choice = -1;
     while (choice != 0)
     {
         printf("== CURRENT : %d ==\n", currentID);
         printf("0:Quit\n1:Point\n2:Line\n3:Square\n4:Rectangle\n5:Circle\n6:Polygon\n");
+        printf("7:Show shapes\n");
         printf("> ");
         scanf("%d", &choice);
 
@@ -36,40 +38,49 @@ int main(int argc, char const *argv[])
         {
         case 1: // Point
             listShapes[currentID] = create_point_shape(1, 2);
+            currentID++;
             break;
 
         case 2: // Line
             listShapes[currentID] = create_line_shape(4,3,2,1);
+            currentID++;
             break;
 
         case 3: // Square
             listShapes[currentID] = create_square_shape(4,2,0);
+            currentID++;
             break;
 
         case 4: // Rectangle
             listShapes[currentID] = create_rectangle_shape(4,3,2,1);
+            currentID++;
             break;
 
         case 5: // Circle
             listShapes[currentID] = create_circle_shape(3,1,4);
+            currentID++;
             break;
 
         case 6: // Polygon
-            listShapes[currentID] = create_polygon_shape(/**/);
+            listShapes[currentID] = create_polygon_shape(nodeList, 6);
+            currentID++;
             break;
         
-        default:
-            currentID--;
-        }
-        currentID++;
+        case 7: // Show shapes
+            printf("== SHAPES ==\n");
+            for (size_t i = 0; i < currentID; i++)
+            {
+                printf("%d - ", i);
+                print_shape(listShapes[i]);
+            }
+            break;
 
-        printf("== SHAPES ==\n");
-        for (size_t i = 0; i < currentID; i++)
-        {
-            printf("%d - ", i);
-            print_shape(listShapes[i]);
+        default:
+            break;
         }
-        printf("\n");
+
+
+        
 
     }
     
