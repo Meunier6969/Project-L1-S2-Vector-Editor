0b159991ebed1913c3a263c0b3b0d3b04233a949
= <antoine.meunier@efrei.net>
2023-04-24 20:56:19 +0200

    'initial' commit

--- area.c
+++ area.c
@@ -1,3 +1,90 @@
-// TODO
+#include <stdlib.h>
+#include <stdio.h>
+#include "area.h"
 
-// test
+Area *create_area(unsigned int width, unsigned int height)
+{
+    Area* area = malloc(sizeof(Area));
+
+    area->height = height;
+    area->width = width;
+ 
+    area->mat = malloc(height * sizeof(int*));
+    for (size_t i = 0; i < height; i++)
+    {
+        area->mat[i] = malloc(width * sizeof(int));
+    }
+    
+    area->shapes = malloc(SHAPE_MAX * sizeof(Shape*));
+    area->nb_shape = 0;
+
+    return area;
+}
+
+void add_shape_to_area(Area *area, Shape *shape)
+{
+    area->shapes[area->nb_shape] = shape;
+    area->nb_shape++;
+}
+
+void initialize_area(Area *area)
+{
+    for (size_t y = 0; y < area->height; y++)
+    {
+        for (size_t x = 0; x < area->width; x++)
+        {
+            area->mat[y][x] = 0;
+        }
+    }
+}
+
+void erase_area(Area *area)
+{
+    for (size_t i = 0; i < area->nb_shape; i++)
+    {
+        delete_shape(area->shapes[i]);
+    }
+    area->nb_shape = 0;
+}
+
+void delete_area(Area *area)
+{
+    erase_area(area);
+    free(area->shapes);
+
+    for (size_t i = 0; i < area->height; i++)
+        free(area->mat[i]);
+    
+    free(area->mat);
+
+    free(area);
+}
+
+void draw_shapes_to_area(Area *area)
+{
+}
+
+void print_area(Area *area)
+{
+    for (size_t y = 0; y < area->height; y++)
+    {
+        for (size_t x = 0; x < area->width; x++)
+        {
+            if (area->mat[y][x])
+                printf("# ");
+            else
+                printf(". ");
+        }
+        printf("\n");
+    }
+}
+
+void list_shapes(Area *area)
+{
+    for (size_t i = 0; i < area->nb_shape; i++)
+    {
+        printf("%d : ", i);
+        print_shape(area->shapes[i]);
+    }
+    printf("\n");
+}
--- area.h
+++ area.h
@@ -1,1 +1,26 @@
-// TODO
+#include "shapes.h"
+
+#define SHAPE_MAX 100
+
+// AREA DEFINTION
+typedef struct area
+{
+    unsigned int width;
+    unsigned int height;
+
+    int **mat;
+
+    Shape **shapes; // List of pointers to Shapes
+    int nb_shape;
+} Area;
+
+// AREA FUNCTIONS
+Area *create_area(unsigned int width, unsigned int height);
+void add_shape_to_area(Area *area, Shape *shape);
+void initialize_area(Area *area);
+void erase_area(Area *area);
+void delete_area(Area *area);
+void draw_shapes_to_area(Area *area);
+void print_area(Area *area);
+
+void list_shapes(Area *area);
