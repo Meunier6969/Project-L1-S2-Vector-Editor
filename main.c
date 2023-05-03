34d3bf8fb4b34de6d5854829103507a587412ffc
= <antoine.meunier@efrei.net>
2023-05-03 10:14:02 +0200

    test of functions

--- main.c
+++ main.c
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "shapes.h"
-#include "draw.h"
 #include "area.h"
 #include "menu.h"
-
-#define WIDTH 32
-#define HEIGHT 20
 
 int main(int argc, char const *argv[]) 
 {
@@ -16,16 +12,18 @@ int main(int argc, char const *argv[])
 
     Shape *pipi = create_point_shape(2, 4);
     Shape *pipi2 = create_point_shape(6, 9);
+    Shape *pipi3 = create_point_shape(-1, 69);
     add_shape_to_area(area, pipi);
     add_shape_to_area(area, pipi2);
+    add_shape_to_area(area, pipi3);
 
     list_shapes(area);
+
+    draw_shapes_to_area(area);
+
     print_area(area);
 
     delete_area(area);
-
-    list_shapes(area);
-    // print_area(area);
 
     return 0;
 }
