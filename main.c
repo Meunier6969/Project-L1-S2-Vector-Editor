d11b3855b45b871700c75f04b006f189f874e7a6
= <antoine.meunier@efrei.net>
2023-05-08 01:08:19 +0200

    bump

--- main.c
+++ main.c
@@ -9,6 +9,7 @@ int main(int argc, char const *argv[])
     Area* area = create_area(32, 20);
     initialize_area(area);
 
+    /* FULLY INSIDE SHAPES */
     Shape *p1 = create_point_shape(1, 2);
     Shape *p2 = create_point_shape(4, 8);
     add_shape_to_area(area, p1);
@@ -19,6 +20,25 @@ int main(int argc, char const *argv[])
 
     Shape *sqr = create_square_shape(7, 25, 5);
     add_shape_to_area(area, sqr);
+
+    Shape *rect = create_rectangle_shape(1, 10, 7, 4);
+    add_shape_to_area(area, rect);
+
+    Shape *cirque = create_circle_shape(10, 15, 5);
+    add_shape_to_area(area, cirque);
+
+    /* PARTLY OUTSIDE SHAPES */
+    Shape *outside_line = create_line_shape(16, 16, 32, 64);
+    add_shape_to_area(area, outside_line);
+
+    Shape *outside_sqr = create_square_shape(10, -5, 12);
+    add_shape_to_area(area, outside_sqr);
+
+    Shape *outside_rect = create_rectangle_shape(3, 20, 16, 3);
+    add_shape_to_area(area, outside_rect);
+
+    Shape *outside_cirque = create_circle_shape(10, 15, 15);
+    add_shape_to_area(area, outside_cirque);
 
     list_shapes(area);
 
