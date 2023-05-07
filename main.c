05ad1cd8b590fa3f00b916101f6c94e75eacf201
= <antoine.meunier@efrei.net>
2023-05-07 20:38:08 +0200

    test of functions

--- main.c
+++ main.c
@@ -7,20 +7,22 @@
 int main(int argc, char const *argv[]) 
 {
     Area* area = create_area(32, 20);
-
     initialize_area(area);
 
-    Shape *pipi = create_point_shape(2, 4);
-    Shape *pipi2 = create_point_shape(6, 9);
-    Shape *pipi3 = create_point_shape(-1, 69);
-    add_shape_to_area(area, pipi);
-    add_shape_to_area(area, pipi2);
-    add_shape_to_area(area, pipi3);
+    Shape *p1 = create_point_shape(1, 2);
+    Shape *p2 = create_point_shape(4, 8);
+    add_shape_to_area(area, p1);
+    add_shape_to_area(area, p2);
+
+    Shape *line = create_line_shape(15, 9, 7, 20);
+    add_shape_to_area(area, line);
+
+    Shape *sqr = create_square_shape(7, 25, 5);
+    add_shape_to_area(area, sqr);
 
     list_shapes(area);
 
     draw_shapes_to_area(area);
-
     print_area(area);
 
     delete_area(area);
