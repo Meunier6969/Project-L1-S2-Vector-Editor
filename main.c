8314e5d235c42b35a5f11a3f3236236b571733b5
= <antoine.meunier@efrei.net>
2023-05-21 20:46:04 +0200

    stream RRRepeat!!! by Gyari

--- main.c
+++ main.c
@@ -3,24 +3,20 @@
 #include "shapes.h"
 #include "area.h"
 #include "menu.h"
+#include "command.h"
 
 int main(int argc, char const *argv[]) 
 {
-    Area* area = create_area(32, 20);
-    initialize_area(area);
+    // Area* area = create_area(32, 20);
+    // initialize_area(area);
 
-    int list[12] = {1, 1, 2, 10, 10, 5, 12, 20, 18, 3, 1, 1};
-    Shape *pppp = create_polygon_shape(list, 6);
+    // Command* cmd = create_command();
+    // read_from_stdin(cmd);
+    // exec_command(cmd);
 
-    add_shape_to_area(area, pppp);
+    // delete_area(area);
 
-
-    list_shapes(area);
-
-    draw_shapes_to_area(area);
-    print_area(area);
-
-    delete_area(area);
+    printf("%d\n", is_number(NULL));
 
     return 0;
 }
